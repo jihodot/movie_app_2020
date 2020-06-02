3847960ad953ad3f37fd33696ec493f59d95d98c
jihod <jihodot@gmail.com>
2020-06-02 22:30:52 +0900

    Updates

---
+++ precache-manifest.27e6d25d858ccb57cb90caafc866f917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4dabc880d3943976de8c1363c5d44b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a4d3d9f29f1f4c11834d",
+    "url": "/movie_app_2020/static/css/main.107edaef.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d3d9f29f1f4c11834d",
+    "url": "/movie_app_2020/static/js/main.5ac4be55.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
